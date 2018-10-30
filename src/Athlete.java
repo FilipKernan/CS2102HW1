4c9378388a1ff651aa0de49d214da31260994f01
Unknown <pilotpatrick2000@gmail.com>
2018-10-29 21:04:49 -0400

    getting good at code (lmao jk)

--- src/Athlete.java
+++ src/Athlete.java
@@ -2,13 +2,9 @@ public class Athlete {
     FinalResult result;
 
     boolean betterSkiier(Athlete competitor) {
-        if (this.result.skiingResult.pointsEarned() > competitor.result.skiingResult.pointsEarned()) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.result.skiingResult.pointsEarned() > competitor.result.skiingResult.pointsEarned();
     }
-    
+
     public boolean betterShooter(Athlete otherAthlete){
         return this.result.shootingResult.pointsEarned() > otherAthlete.result.shootingResult.pointsEarned();
     }
