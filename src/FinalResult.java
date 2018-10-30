a93423c8cef9d364730ae52f58a0075bae5eee03
Unknown <pilotpatrick2000@gmail.com>
2018-10-29 20:47:51 -0400

    added modified based on finishing position

--- src/FinalResult.java
+++ src/FinalResult.java
@@ -3,6 +3,16 @@ public class FinalResult {
     SkiingResult skiingResult;
 
     double finalScore() {
-        return skiingResult.pointsEarned() + skiingResult.getPenalties() + shootingResult.getPenalties();
+        int placeMod = 0
+        if (skiingResult.finishPlace == 1) {
+            placeMod = 10;
+        } else if (skiingResult.finishPlace == 2) {
+            placeMod = 7;
+        } else if (skiingResult.finishPlace == 3) {
+            placeMod = 3;
+        } else if (skiingResult.finishPlace == 4) {
+            placeMod = 1;
+        }
+        return skiingResult.pointsEarned() + skiingResult.getPenalties() + shootingResult.getPenalties() - placeMod;
     }
 }
