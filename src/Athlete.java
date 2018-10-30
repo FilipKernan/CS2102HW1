7ccde48aff7b1839750228037725f7a3523773be
Filip Kernan <filip.kernan@gmail.com>
2018-10-29 21:06:15 -0400

    added hasBeaten

--- src/Athlete.java
+++ src/Athlete.java
@@ -1,7 +1,7 @@
 public class Athlete {
     FinalResult result;
 
-    boolean betterSkiier(Athlete competitor) {
+    public boolean betterSkiier(Athlete competitor) {
         if (this.result.skiingResult.pointsEarned() > competitor.result.skiingResult.pointsEarned()) {
             return true;
         } else {
@@ -12,5 +12,9 @@ public class Athlete {
     public boolean betterShooter(Athlete otherAthlete){
         return this.result.shootingResult.pointsEarned() > otherAthlete.result.shootingResult.pointsEarned();
 
+    }
+
+    public boolean hasBeaten(Athlete competitor){
+        return this.betterShooter(competitor) || this.betterSkiier(competitor);
     }
 }
