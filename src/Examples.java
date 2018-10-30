5c5bc09157fa3909639c02ba7791896e6afb6f3d
Unknown <pilotpatrick2000@gmail.com>
2018-10-29 22:03:13 -0400

    Shoob

--- src/Examples.java
+++ src/Examples.java
@@ -4,6 +4,21 @@ import static org.junit.Assert.*;
 
 public class Examples {
 
+    SkiingResult skiing1 = new SkiingResult(120, 125, 110, 130, 1,3);
+    SkiingResult skiing2 = new SkiingResult(120, 125,115,130,2,3);
+    SkiingResult skiing3 = new SkiingResult(125,125,115,130,3,0);
+    SkiingResult skiing4 = new SkiingResult(125 ,130,115,130, 4, 1);
+    SkiingResult skiing5 = new SkiingResult(140, 200, 170, 185, 13, 12);
+
+    @Test
+    public void testSkiingPoints() {
+        assertTrue(skiing1.pointsEarned() == 485);
+    }
+    @Test
+    public void testSkiingPenalties() {
+        assertTrue(skiing5.getPenalties() == 65);
+    }
+
     ShootingRound[] rounds1 = {
             new ShootingRound(5),new ShootingRound(5),
             new ShootingRound(5),new ShootingRound(5)
