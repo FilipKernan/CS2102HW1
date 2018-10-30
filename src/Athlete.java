dedd83e08afff4a1ca6cd27e2c8704a9b5aba02d
Unknown <pilotpatrick2000@gmail.com>
2018-10-29 20:55:46 -0400

    betterSkier method in Athlete class

--- src/Athlete.java
+++ src/Athlete.java
@@ -1,5 +1,11 @@
 public class Athlete {
     FinalResult result;
 
-
+    boolean betterSkiier(Athlete competitor) {
+        if (this.result.skiingResult.pointsEarned() > competitor.result.skiingResult.pointsEarned()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
