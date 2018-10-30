dce38905e3d77b9f4b3ff33daffd83ffeb37e8fd
Unknown <pilotpatrick2000@gmail.com>
2018-10-29 21:13:43 -0400

    making things public

--- src/Athlete.java
+++ src/Athlete.java
@@ -2,7 +2,7 @@ public class Athlete {
     FinalResult result;
 
 
-    boolean betterSkiier(Athlete competitor) {
+    public boolean betterSkiier(Athlete competitor) {
         return this.result.skiingResult.pointsEarned() < competitor.result.skiingResult.pointsEarned();
 
     }
