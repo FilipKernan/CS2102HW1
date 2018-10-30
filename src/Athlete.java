8aeba9cf7bb778c701a390d45d9363f9d6d89538
Filip Kernan <filip.kernan@gmail.com>
2018-10-29 20:51:58 -0400

    betterShooter

--- src/Athlete.java
+++ src/Athlete.java
@@ -2,4 +2,9 @@ public class Athlete {
     FinalResult result;
 
 
+    public boolean betterShooter(Athlete otherAthlete){
+        return this.result.shootingResult.pointsEarned() > otherAthlete.result.shootingResult.pointsEarned();
+    }
+
+
 }
