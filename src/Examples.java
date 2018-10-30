7b1866430665bc1c7e70e6fb24c12aa7e41a4e08
Filip Kernan <filip.kernan@gmail.com>
2018-10-29 22:02:16 -0400

    added tests for shooting

--- src/Examples.java
+++ src/Examples.java
@@ -4,24 +4,41 @@ import static org.junit.Assert.*;
 
 public class Examples {
 
-    ShootingRound[] rounds1 = {
-            new ShootingRound(5),new ShootingRound(5),
-            new ShootingRound(5),new ShootingRound(5)
-    };
-    ShootingRound[] rounds2 = {
-            new ShootingRound(0),new ShootingRound(0),
-            new ShootingRound(0),new ShootingRound(0)
-    };
-    ShootingRound[] rounds3 = {
-            new ShootingRound(3),new ShootingRound(5),
-            new ShootingRound(4),new ShootingRound(5)
-    };
-    ShootingResult shooting1 = new ShootingResult(rounds1);
-    ShootingResult shooting2 = new ShootingResult(rounds2);
-    ShootingResult shooting3 = new ShootingResult(rounds3);
+    ShootingRound roundMax = new ShootingRound(5);
+    ShootingRound roundMin = new ShootingRound(0);
+    ShootingRound roundVar = new ShootingRound(2);
+
+
+    ShootingResult shooting1 = new ShootingResult(roundMax,roundMax,roundMax,roundMax);
+    ShootingResult shooting2 = new ShootingResult(roundMin,roundMin,roundMin,roundMin);
+    ShootingResult shooting3 = new ShootingResult(roundVar,roundVar,roundVar,roundVar);
 
     @Test
-    public void testPointsEarned(){
-
+    public void testPointsEarnedMax(){
+        assertTrue(shooting1.pointsEarned() == 20);
     }
+    @Test
+    public void testPointsEarnedMin(){
+        assertTrue(shooting2.pointsEarned() == 0);
+    }
+
+    @Test
+    public void testPointsEarnedVar(){
+        assertTrue(shooting3.pointsEarned() == 8);
+    }
+
+    @Test
+    public void testPenaltiesMax(){
+        assertTrue(shooting2.getPenalties() == 20 * 60);
+    }
+    @Test
+    public void testPenaltiesMin(){
+        assertTrue(shooting1.getPenalties() == 0);
+    }
+
+    @Test
+    public void testPenaltiesVar(){
+        assertTrue(shooting3.getPenalties() == 12 * 60);
+    }
+
 }
--- src/ShootingResult.java
+++ src/ShootingResult.java
@@ -1,16 +1,18 @@
 public class ShootingResult implements IEvent{
-    ShootingRound[] rounds = new ShootingRound[4];
+    ShootingRound round1;
+    ShootingRound round2;
+    ShootingRound round3;
+    ShootingRound round4;
 
-    public ShootingResult(ShootingRound[] rounds) {
-        this.rounds = rounds;
+    public ShootingResult(ShootingRound round1, ShootingRound round2, ShootingRound round3, ShootingRound round4) {
+        this.round1 = round1;
+        this.round2 = round2;
+        this.round3 = round3;
+        this.round4 = round4;
     }
 
     public double pointsEarned() {
-        int sum = 0;
-        for (ShootingRound round :rounds) {
-            sum = sum + round.numOfHits;
-        }
-        return sum;
+        return round1.numOfHits + round2.numOfHits + round3.numOfHits + round4.numOfHits;
     }
 
 
